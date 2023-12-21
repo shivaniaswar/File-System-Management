6ebcab9156870d905c48a3bb96f2ec2ecd216e94
shivani <shivaniaswar@gmail.com>
2023-12-21 20:35:25 +0530

    new feature added

--- File.java
+++ File.java
@@ -12,3 +12,7 @@ public Static void main(String[] args){
   System.out.println();
   }
 
+public static void addition(int a, int b){
+int c=a+b;
+System.out.println("The addition of given number is "+c);
+}
